f0f22b44810e500ccf389220ff99dd897e99c55a
shigh <highscot@gmail.com>
2014-11-24 11:44:15 -0600

    Added a second iteration to test case.
    To make sure reset is working correctly

--- code/tests/test_region.cpp
+++ code/tests/test_region.cpp
@@ -72,6 +72,39 @@ BOOST_AUTO_TEST_CASE( region_time_step )
 
 	}
 
+	// Reset and run for another iteration
+	region.reset();
+
+	for(int i=0; i<K; i++)
+		region.set_dt(i+K+1, i);
+		
+	for(int i=0; i<K; i++)
+		region.time_step_chunk();
+
+	for(int i=0; i<K; i++)
+	{
+		auto expected = std::vector<double>(region.get_chunk_size(i)*N, i+K+1);
+		// x0 does not change, so chunk 0 will have its first N values still
+		// set to 1
+		if(i==0)
+			for(int j=0; j<N; j++)
+				expected[j] = 1;
+
+		auto bndy = region.get_boundary(EAST, i);
+		BOOST_CHECK(std::equal(expected.begin(), expected.end(), bndy.begin()));
+
+		bndy = region.get_boundary(WEST, i);
+		BOOST_CHECK(std::equal(expected.begin(), expected.end(), bndy.begin()));
+
+		bndy = region.get_boundary(NORTH, i);
+		BOOST_CHECK(std::equal(expected.begin(), expected.end(), bndy.begin()));
+
+		bndy = region.get_boundary(SOUTH, i);
+		BOOST_CHECK(std::equal(expected.begin(), expected.end(), bndy.begin()));
+
+	}
+
+
 }
 
 
